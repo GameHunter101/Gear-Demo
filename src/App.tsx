b1335dbf3863ac34237327696a9a0b5bbcbe8ef8
GameHunter101 <liorscarmeli@gmail.com>
2023-01-30 16:18:31 -0500

    Added gear spinning

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
-import { useState, useEffect, useRef, RefObject } from 'react'
-import './App.css'
-import Gear from './Gear'
+import { useState, useEffect, useRef, RefObject } from "react";
+import "./App.css";
+import Gear, { RefType } from "./Gear";
 
 function App() {
-	const [gears, setGears] = useState<{ teeth: number, diameter: number }[]>([{ teeth: 5, diameter: 5 }]);
+	const [gears, setGears] = useState<{ teeth: number, diameter: number, num: number }[]>([{ teeth: 5, diameter: 5, num: 0 }]);
 	const [teethCount, setTeethCount] = useState(18);
 	const [pitchDiameter, setPitchDiameter] = useState(20);
+	const [rpm, setRpm] = useState(60);
 	const [selected, setSelected] = useState<number>();
+
+	const gearRefs = useRef<(RefType|null)[]>([]);
 
 	const svgRef = useRef<SVGSVGElement>(null);
 
 	const handleClick = (e: MouseEvent) => {
 		if (e.target instanceof SVGPathElement) {
-			const index = Array.from(svgRef.current!.children).indexOf(e.target);
+			const index = Array.from(svgRef.current!.children).indexOf(e.target.parentElement!.parentElement!);
 			setSelected(index);
 			// svgRef.current?.children[selected].setAttribute("fill", "#f00");
 		} else {
-			setSelected(undefined);
+			if ((e.target as HTMLDivElement).className.includes("App")) {
+				setSelected(undefined);
+			}
 		}
 	}
 
@@ -24,10 +29,10 @@ function App() {
 		window.addEventListener("click", handleClick);
 
 		gears.map((_e, i) => {
-			svgRef.current!.children.item(i)?.setAttribute("fill-opacity", "0");
+			svgRef.current!.children.item(i)!.children[0].children[0].setAttribute("fill-opacity", "0");
 		})
 		if (selected != undefined) {
-			svgRef.current!.children.item(selected)?.setAttribute("fill-opacity", ".3");
+			svgRef.current!.children.item(selected)!.children[0].children[0].setAttribute("fill-opacity", ".3");
 		}
 
 
@@ -36,24 +41,48 @@ function App() {
 		}
 	}, [selected])
 
+	const gearComponents = gears.map((gear, i) => (
+		<Gear teeth={gear.teeth} diameter={gear.diameter} id={i} key={i} num={gear.num} ref={e=>gearRefs.current.push(e)} select={(id) => {
+			setSelected(id);
+		}} />
+	));
+
 
 	return (
 		<div className="App relative">
 			<div className="fixed top-0 w-full h-12 bg-[#262626] flex items-center">
+				{selected == undefined ?
+					<form className="mx-auto grid grid-cols-3 gap-2" onSubmit={(e) => {
+						e.preventDefault();
+						setGears([...gears, { teeth: teethCount, diameter: pitchDiameter, num: 0 }]);
+					}}>
+						<div>
+							<label htmlFor="numTeeth" className="pr-2 text-white">Number of Teeth: </label>
+							<input type="number" id="numTeeth" value={teethCount} className="w-12 bg-transparent focus:outline-none placeholder:text-gray-300 text-gray-300 border-b-[1px]  border-b-gray-700" onChange={e => setTeethCount(e.target.valueAsNumber)} />
+						</div>
+						<div>
+							<label htmlFor="diameter" className="pr-2 text-white">Pitch Diameter (cm): </label>
+							<input type="number" id="diameter" value={pitchDiameter} className="w-12 bg-transparent focus:outline-none placeholder:text-gray-300 text-gray-300 border-b-[1px]  border-b-gray-700" onChange={e => setPitchDiameter(e.target.valueAsNumber)} />
+						</div>
+						<span className="mx-2">
+							<button type="submit" className="bg-[#202020] text-white px-4 rounded-full py-1 shadow-sm shadow-[#0000005F] hover:bg-[#A0A0A0] hover:text-[#202020] transition-all duration-100">Make Gear</button>
+						</span>
+					</form>
+					:
+					<p></p>
+				}
 				<form className="mx-auto grid grid-cols-3 gap-2" onSubmit={(e) => {
 					e.preventDefault();
-					setGears([...gears, { teeth: teethCount, diameter: pitchDiameter }]);
+					if (gears && selected != undefined) {
+						gearRefs.current[selected]?.retrieveSpeed(60/rpm);
+					}
 				}}>
 					<div>
-						<label htmlFor="numTeeth" className="pr-2 text-white">Number of Teeth: </label>
-						<input type="number" id="numTeeth" value={teethCount} className="w-12 bg-transparent focus:outline-none placeholder:text-gray-300 text-gray-300 border-b-[1px]  border-b-gray-700" onChange={e => setTeethCount(e.target.valueAsNumber)} />
-					</div>
-					<div>
-						<label htmlFor="diameter" className="pr-2 text-white">Pitch Diameter (cm): </label>
-						<input type="number" id="diameter" value={pitchDiameter} className="w-12 bg-transparent focus:outline-none placeholder:text-gray-300 text-gray-300 border-b-[1px]  border-b-gray-700" onChange={e => setPitchDiameter(e.target.valueAsNumber)} />
+						<label htmlFor="rpm" className="pr-2 text-white">Gear RPM</label>
+						<input type="number" id="rpm" value={rpm} className="w-12 bg-transparent focus:outline-none placeholder:text-gray-300 text-gray-300 border-b-[1px]  border-b-gray-700" onChange={e => setRpm(e.target.valueAsNumber)} />
 					</div>
 					<span className="mx-2">
-						<button type="submit" className="bg-[#202020] text-white px-4 rounded-full py-1 shadow-sm shadow-[#0000005F] hover:bg-[#A0A0A0] hover:text-[#202020] transition-all duration-100">Make Gear</button>
+						<button type="submit" className="bg-[#202020] text-white px-4 rounded-full py-1 shadow-sm shadow-[#0000005F] hover:bg-[#A0A0A0] hover:text-[#202020] transition-all duration-100">Set RPM</button>
 					</span>
 				</form>
 			</div>
@@ -64,16 +93,7 @@ function App() {
 				height={"100vh"}
 				ref={svgRef}
 			>
-
-				{gears.map((gear, i) => {
-					return <Gear teeth={gear.teeth} diameter={gear.diameter} id={i} key={i} select={(id) => {
-						setSelected(id);
-						// console.log(selected);
-						// setSelected(svgRef.current!.children[id]);
-						// svgRef.current!.children[id].;
-						// gear.current!.setAttribute("fill","#f00");
-					}} />
-				})}
+				{gearComponents}
 			</svg>
 		</div>
 	)
--- src/Gear.tsx
+++ src/Gear.tsx
@@ -1,4 +1,4 @@
-import React, { ForwardedRef, RefObject, useEffect, useRef, useState } from "react";
+import React, { ForwardedRef, forwardRef, Ref, RefObject, useEffect, useImperativeHandle, useRef, useState } from "react";
 import { ReactSVG } from "react-svg";
 // import init, {make_gear} from "gear-calc";
 import init, { make_gear, test } from "gear-calc";
@@ -8,37 +8,67 @@ interface GearProps {
 	teeth: number,
 	diameter: number,
 	id: number,
-	select: (id:number)=>void
+	select: (id: number) => void,
+	num: number
 }
 
-const Gear = (props: GearProps) => {
+export interface RefType {
+	retrieveSpeed: (num:number)=>void
+}
+
+const Gear = forwardRef<RefType,GearProps>((props: GearProps, ref) => {
 	const [points, setPoints] = useState("");
+	const [speed,setSpeed] = useState(10);
 
-	const ref = useRef<SVGPathElement>(null);
+	const pathRef = useRef<SVGPathElement>(null);
 
+	useEffect(() => {
+		console.log(speed);
+		// pathRef.current?.classList.add("animate-[spin_5s_linear_infinite]")
+	
+	}, [speed]);
+
+	const retrieveSpeed = (num:number)=>{
+		setSpeed(num);
+	};
+	
+	useImperativeHandle(ref,()=>({retrieveSpeed}))
+
+	// if (ref.current) {
+	// 	console.log(ref.current["offsetWidth" as never]);
+	// }
 	const pixelToCm = 0.026458;
 	init().then(() => {
 		setPoints(make_gear(props.teeth, props.diameter / pixelToCm));
 	});
+	const className = `pointer-events-auto origin-center animate-[spin_${speed}s_linear_infinite]`;
 	return (
 		<>
-
-			<Draggable nodeRef={ref as any}>
-				<path
-					strokeWidth={"1"}
-					stroke="black"
-					fill="#000"
-					fillOpacity={0}
-					d={points}
-					className="pointer-events-auto"
-					// transform={`translate(${offsetX + " " + offsetY})`}
-					ref={ref}
-					onClick={()=>props.select(props.id)}
-				>
-				</path>
+			<Draggable nodeRef={pathRef as any}>
+				<foreignObject width={(props.diameter + 3 * (props.diameter / props.teeth)) / pixelToCm} height={(props.diameter + 3 * (props.diameter / props.teeth)) / pixelToCm}>
+					<svg
+						xmlns="<http://www.w3.org/2000/svg>"
+						className="overflow-visible w-fit h-fit rounded-full pointer-events-none"
+						width={(props.diameter + 3 * (props.diameter / props.teeth)) / pixelToCm}
+						height={(props.diameter + 3 * (props.diameter / props.teeth)) / pixelToCm}>
+						<path
+							strokeWidth={"1"}
+							stroke="black"
+							fill="#000"
+							fillOpacity={0}
+							d={points}
+							className={className}
+							style={{ transformBox: "fill-box" }}
+							// transform={`translate(${offsetX + " " + offsetY})`}
+							onClick={() => props.select(props.id)}
+							ref={pathRef}
+						>
+						</path>
+					</svg>
+				</foreignObject>
 			</Draggable>
 		</>
 	);
-};
+});
 
 export default Gear;
